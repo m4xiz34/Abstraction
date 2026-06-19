8b632b20cdad2cbb898664781d49524a2242d37d
m4xiz34 <maxisacroissant@gmail.com>
2026-06-19 18:47:59 +1000

    Create country.py

---
+++ country.py
@@ -0,0 +1,19 @@
+class India():
+    def capital(self):
+        print("New Delhi is the capital city of India.")
+        
+    def language(self):
+        print("Hindi is the most common langauge in India, though there's many more...")
+        
+    def type(self):
+        print("India is a deveoping counrty as of the moment...")
+        
+class Australia():
+    def capital(self):
+        print("\nCanberra is the capital city of Australia.")
+        
+    def language(self):
+        print("English is the most common langauge in Australia!")
+        
+    def type(self):
+        print("Australia is a highly developed counrty.")
